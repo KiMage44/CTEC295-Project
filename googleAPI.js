2f25d089643768111a8fa1c7148e24b0f78dfdc5
Trenton Choate <38508613+KiMage44@users.noreply.github.com>
2020-11-07 19:56:11 -0600

    Implemented basic Working functionality of image uploads, allowing to upload png files from computer to the user's drive.

--- googleAPI.js
+++ googleAPI.js
@@ -1,8 +1,9 @@
-var CLIENT_ID = '';
-var API_KEY = '';
+var CLIENT_ID = '1082519719763-p93jidt452epb2u7gvathglruul0f744.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDYwGr98qKbm9-NbSEFkyXsHLMW-XECHMc';
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 var SCOPES = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.install https://www.googleapis.com/auth/drive';
-
+var Window = "";
+var LoginCredentials;
 function handleClientLoad() { gapi.load('client:auth2', initClient); }
 function initClient() {
   gapi.client.init({
@@ -12,7 +13,8 @@ function initClient() {
     scope: SCOPES
   }).then(function () {
     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
+     LoginCredentials = gapi.auth2.getAuthInstance();
+     googleUserAccount = LoginCredentials.currentUser.get();
     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
   }, function(error) {
     console.log(JSON.stringify(error, null, 2));
@@ -21,37 +23,38 @@ function initClient() {
 function updateSigninStatus(isSignedIn) {}
 function handleAuthClick(event) { gapi.auth2.getAuthInstance().signIn(); }
 function handleSignoutClick(event) { gapi.auth2.getAuthInstance().signOut(); }
+function initialContentSetup(window){
+  Window = window;
+  Window.currentFile = '';
+  var picture = Window.document.getElementById("picture");
+  var fileSelect = Window.document.getElementById("fileInput").addEventListener("change", event => {
+      var file = event.target.files[0];
+      changeCurrentFile(file);
+      var reader = new FileReader();
+      //reader.addEventListener('load', event => { picture.src = event.target.result; });
+      reader.readAsDataURL(file);
+    });
+};
 
-async function createFile(){
-  var fileCont = "C:\\Users\\Admin\\Pictures\\acceptance.png";
-  var file = new Blob([fileCont], {type: 'image/png'});
-  var metadata = {
-    'name': 'soup',
-  'mimeType':'image/png'
-  };
-  var accesstok = gapi.auth.getToken().access_token;
-
-  var bodyContent = new URLSearchParams();
-  bodyContent.append('metadata', new Blob([JSON.stringify(metadata)], { type: "application/json"}));
-  bodyContent.append("uploadType","media");
-
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization","Bearer "+accesstok);
-  myHeaders.append("Content-Type","image/png");
-  myHeaders.append("Content-Length","343712");
-
-  var request = new Request("https://www.googleapis.com/upload/drive/v3/files", {
+function postFileToDrive(){
+  var file = Window.currentFile;
+  var url = new URL("https://www.googleapis.com/upload/drive/v3/files");
+  url.searchParams.append("uploadType","media");
+  var request = new Request(url, {
     method: "POST",
-    headers: myHeaders,
-    body: bodyContent
+    headers: {'Content-Type':"image/png", 'Content-Length':file.size,
+    'Authorization':"Bearer "+googleUserAccount.wc.access_token},
+    body: file,
   });
-  let accessData = await fetchRequest(request);
-  return accessData;
-}
+  fetchRequest(request);
+};
 async function fetchRequest(request){
   let response = await fetch(request);
   if(response.status >=200 && response.status < 300)
-  {return response.json();}
+  {return response;}
   else
   {return Promise.reject(new Error(response.statusText));}
+};
+function changeCurrentFile(file){
+  Window.currentFile = file;
 };
