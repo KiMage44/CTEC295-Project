d35e2a9eba2978fdfb3924c058805b6b918cece9
Trenton Choate <38508613+KiMage44@users.noreply.github.com>
2020-11-09 12:38:12 -0600

    Improved functionality with image load/editing, files can now be loaded into canvas, edited with current functions, and saved. Note: Currently only supports png files.

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
 var Window = "";
-var canvas = "";
-var stylus = "";
 function initiate(value){
   Window = value;
-  canvas = Window.document.getElementById("drawingCanvas");
-  stylus = canvas.getContext("2d");
-  stylus.fillStyle = "#FFFFFF";
+  Window.canvas = Window.document.getElementById("drawingCanvas");
+  Window.stylus = canvas.getContext("2d");
+  Window.stylus.fillStyle = "#FFFFFF";
+  drawShape('square',0,0,Window.canvas.width,Window.canvas.height);
 };
 function setColor(value){
-  stylus.fillStyle = value;
+  Window.stylus.fillStyle = value;
 };
 function drawShape(type,x,y,width,height){
   switch(type){
     case "circle":
-      stylus.beginPath();
-      stylus.arc(x,y,width,0,2*Math.PI);
-      stylus.fill();
+      Window.stylus.beginPath();
+      Window.stylus.arc(x,y,width,0,2*Math.PI);
+      Window.stylus.fill();
       break;
     case "rectangle":
-      stylus.fillRect(x,y,width,height);
+      Window.stylus.fillRect(x,y,width,height);
       break;
     }
 };
-function saveDrawingToDrive(){
-  var image = canvas.toDataURL();
-  console.log(image);
-  return image;
-};
 function loadImagetoCanvas(){
-   Window.image = new Image();
+  Window.image = new Image();
   Window.image.onload = function(){
-    stylus.drawImage(Window.image,0,0);
-    stylus.fillStyle = "rgba(200, 0, 0, 0.5)";
-    stylus.fillRect(0, 0, 500, 500);
+    Window.canvas.width = Window.image.width;
+    canvas.height = Window.image.height;
+    Window.stylus.drawImage(Window.image,0,0);
   };
   var reader = new FileReader();
   reader.onload = function(event){
@@ -40,3 +34,7 @@ function loadImagetoCanvas(){
   };
   reader.readAsDataURL(Window.currentFile);
 };
+function wipeCanvas(){
+  Window.stylus.fillStyle = "#FFFFFF";
+  drawShape('rectangle',0,0,Window.canvas.width,Window.canvas.height);
+}
