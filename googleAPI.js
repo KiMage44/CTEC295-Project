ffdbd5cc10cbe604e8059740c3819953baaaf6af
Trenton Choate <38508613+KiMage44@users.noreply.github.com>
2020-11-09 12:38:27 -0600

    d

--- googleAPI.js
+++ googleAPI.js
@@ -1,5 +1,5 @@
-var CLIENT_ID = '1082519719763-p93jidt452epb2u7gvathglruul0f744.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyCMU95EgLXtueBNYhAl4UoGeCxB232uNUI';
+var CLIENT_ID = '';
+var API_KEY = '';
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 var SCOPES = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.install https://www.googleapis.com/auth/drive';
 var Window = "";
