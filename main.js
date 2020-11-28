1ad08e87cd28d71f9268e9a600db7553a95349b5
Trenton Choate <38508613+KiMage44@users.noreply.github.com>
2020-11-27 21:28:30 -0600

    Some basic support for mouse drawing that I have gotten to correctly work. Note: Resizing the window will result in destruction of current canvas.

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ function initiate(value){
   Window = value;
   Window.canvas = Window.document.getElementById("drawingCanvas");
   Window.stylus = canvas.getContext("2d");
-  Window.stylus.fillStyle = "#FFFFFF";
-  drawShape('square',0,0,Window.canvas.width,Window.canvas.height);
+  Window.stylus.fillStyle = "#000000";
+  Window.document.getElementById("drawingCanvas").addEventListener("click", function(e){registerClick(e);});
+  Window.document.getElementById("drawingCanvas").addEventListener("mousemove", function(e){registerMouseMove(e);});
+  Window.document.getElementById("drawingCanvas").addEventListener("mousedown",function(e){registerMouseDown(e);});
+  Window.document.getElementById("drawingCanvas").addEventListener("mouseup",function(e){registerMouseUp(e);});
+  Window.addEventListener("resize",function(e){resizeCanvas(e);});
 };
 function setColor(value){
   Window.stylus.fillStyle = value;
+  console.log(Window.stylus.fillStyle);
 };
 function drawShape(type,x,y,width,height){
   switch(type){
@@ -37,4 +42,30 @@ function loadImagetoCanvas(){
 function wipeCanvas(){
   Window.stylus.fillStyle = "#FFFFFF";
   drawShape('rectangle',0,0,Window.canvas.width,Window.canvas.height);
+}
+function registerClick(e){
+  console.log(e.clientX);
+  console.log(e.clientY);
+}
+function registerMouseMove(e){
+  var drawPosX = e.clientX - Window.innerWidth*0.1;
+  var drawPosY = e.clientY - Window.innerHeight*0.12;
+  if(Window.mouseDown){
+    console.log("Draw X: "+ drawPosX);
+    console.log("Draw Y: "+ drawPosY);
+    drawShape('rectangle',drawPosX,drawPosY,2,2);
+  }
+}
+function registerMouseDown(e){
+  Window.mouseDown = true;
+  console.log("Mousedown = "+Window.mouseDown);
+}
+function registerMouseUp(e){
+  Window.mouseDown = false;
+  console.log("Mousedown = "+Window.mouseDown);
+}
+async function resizeCanvas(e){
+  console.log("Window resized, recreating canvas.");
+  Window.document.getElementById("drawingCanvas").width = this.innerWidth*0.8;
+  Window.document.getElementById("drawingCanvas").height = this.innerHeight*0.8;
 }
